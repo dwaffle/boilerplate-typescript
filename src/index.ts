a8c04b26c437f8edf0ac1dbc68d93b7cc3521c35
Bradley Holbrook <bholbrook73@gmail.com>
2020-09-14 19:10:30 -0700

    Prettier Installation and Setup

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
-
-function helloWorld( message:string ){
-
+function helloWorld(message: string) {
     console.log("Hello World");
 
-    let updatedMessage;
+    let updatedMessage = { f: 1, b: 2 };
 
     return 0;
-
 }
 
 helloWorld("goodbye world");
