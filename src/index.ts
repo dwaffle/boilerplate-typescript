a8e00594fc991c3fc5e962b42b881802b6a8c01d
Bradley Holbrook <bradley@praetorianstudio.com>
2020-09-17 10:16:57 -0700

    Updated Package Calls and Expanded Readme

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-function helloWorld(message: string) {
-    console.log("Hello World!");
-
-    const updatedMessage = { f: 1, b: 2 };
-
-    return 0;
-}
-
-helloWorld("goodbye world");
+console.log("Hello World");
